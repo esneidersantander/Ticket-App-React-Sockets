1db6bf721297e6efdd06bb25397f78e04ec7c2cc
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-03-11 15:29:07 -0500

    crear ticket page

--- src/pages/Escritorio.js
+++ src/pages/Escritorio.js
@@ -1,9 +1,63 @@
 import React from 'react'
+import { Row, Col, Typography, Button, Divider} from 'antd'
+import { CloseCircleOutlined, RightOutlined } from '@ant-design/icons'
 
+
+const {Title, Text} = Typography
 export const Escritorio = () => {
+
+    const salir = () =>{
+        console.log('salir');
+    } 
+    const siguienteTicket = () =>{
+        console.log('sivuiente ticket');
+    } 
+
     return (
-        <div>
-            Escritorio
-        </div>
+        <>
+            <Row>
+                <Col span={20}>
+                        <Title level={2}> Esneider</Title>
+                        <Text>Usted está trabajando en el escritorio:</Text>
+                        <Text type="success">4</Text>
+                </Col>
+                <Col span={4} align="right">
+                    <Button
+                        shape="round"
+                        type="danger"
+                        onClick={salir}
+                    >
+                        <CloseCircleOutlined/>
+                        Salir
+                    </Button>
+                </Col>
+            </Row>
+            <Divider/>
+            <Row>
+                <Col>
+                    <Text>
+                        Está antendiendo el ticket número:
+                    </Text>
+                    <Text
+                        style={{fontSize:30}}
+                        type="danger"
+                    >
+
+                    </Text>
+                </Col>
+            </Row>
+            <Row>
+                <Col offset={18} spoan={6} align="right">
+                    <Button
+                        onClick={siguienteTicket}
+                        shape="round"
+                        type="primary"
+                    >
+                        <RightOutlined/>
+                        Siguiente
+                    </Button>
+                </Col>
+            </Row>
+        </>
     )
 }
--- src/pages/Ingresar.js
+++ src/pages/Ingresar.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Form, Input, Button, InputNumber } from 'antd';
+import { Form, Input, Button, InputNumber, Typography, Divider } from 'antd';
 import { SaveOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router';
 const layout = {
     labelCol: {
         span: 8,
@@ -16,57 +17,68 @@ const tailLayout = {
     },
 };
 
+const {Title, Text} = Typography;
+
 export const Ingresar = () => {
+    const history = useHistory();
+
     const onFinish = (values) => {
         console.log('Success:', values);
+        history.push('/escritorio');
     };
+
 
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
     return (
-        <Form
-            {...layout}
-            name="basic"
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-        >
-            <Form.Item
-                label="Nombre del agente"
-                name="agente"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Por favor ingrese su nombre',
-                    },
-                ]}
+        <>
+            <Title level={2}>Ingresar</Title>
+            <Text>Ingrese su nombre y número de escritorio</Text>
+            <Divider/>
+            <Form
+                {...layout}
+                name="basic"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
             >
-                <Input />
-            </Form.Item>
+                <Form.Item
+                    label="Nombre del agente"
+                    name="agente"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Por favor ingrese su nombre',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-            <Form.Item
-                label="Escritorio"
-                name="escritorio"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Por favor ingrese el número de escritorio',
-                    },
-                ]}
-            >
-                <InputNumber min={1} max={99}/>
-            </Form.Item>
+                <Form.Item
+                    label="Escritorio"
+                    name="escritorio"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Por favor ingrese el número de escritorio',
+                        },
+                    ]}
+                >
+                    <InputNumber min={1} max={99}/>
+                </Form.Item>
 
 
-            <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit">
-                    <SaveOutlined />
-                    Ingresar
-                </Button>
-            </Form.Item>
-        </Form>
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit" shape="round">
+                        <SaveOutlined />
+                        Ingresar
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
     )
 }
